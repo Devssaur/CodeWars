be5bd4ed73945a1df399b1941e86a67ce3033da7
G.O.A <guilherme.oliveira.antonio@gmail.com>
2021-06-01 14:46:46 -0300

    terceiro

---
+++ 8kyu(facil)/terceiro.py
@@ -0,0 +1,11 @@
+#https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/python/60b5ad0408092600253ecba7
+
+def get_count(input_str):
+    num_vowels = 0
+    for item in list(input_str):
+        if item == 'a' or  item == 'e' or  item == 'i' or  item == 'o' or  item == 'u' :
+            num_vowels += 1
+
+    return num_vowels
+
+print(get_count("eit"))
